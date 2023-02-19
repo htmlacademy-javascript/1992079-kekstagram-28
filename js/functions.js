ae8bdb00fe5b396d946ce71fd682370b81307a5b
4rutemu <artyom.denega@gmail.com>
2023-02-19 16:47:31 +0300

    Task 2.28 lint fix

--- js/functions.js
+++ js/functions.js
@@ -1,40 +1,44 @@
 function checkStringLength(str, strLength) {
-    return str.length <= strLength;
+  return str.length <= strLength;
 }
 
 function checkPalindrome(str) {
-    let strToCheck = '';
+  let strToCheck = '';
 
-    str = str.replaceAll(' ', '');
+  str = str.replaceAll(' ', '');
 
-    for (let i = str.length - 1; i >= 0; i--) {
-        strToCheck += str[i];
-    }
+  for (let i = str.length - 1; i >= 0; i--) {
+    strToCheck += str[i];
+  }
 
-    return str === strToCheck;
+  return str === strToCheck;
 }
 
 function getNumbersFromString(str) {
-    str = String(str);
-    
-    let number = '';
-    
-    for (let i = 0; i < str.length; i++) {
-        if (Number(str[i]) || str[i] === '0') {
-            number += str[i];
-        }
-    }
+  str = String(str);
 
-    return Number(number);
+  let number = '';
+
+  for (let i = 0; i < str.length; i++) {
+    if (Number(str[i]) || str[i] === '0') {
+      number += str[i];
+    }
+  }
+
+  return Number(number);
 }
 
 function addSymbols(str, strLength, addingStr) {
-    
-    while (str.length < strLength) {
-        
-        str = addingStr.slice(0, strLength - str.length) + str;
-        
-    }
 
-    return str;
+  while (str.length < strLength) {
+    str = addingStr.slice(0, strLength - str.length) + str;
+  }
+
+  return str;
 }
+
+
+checkStringLength('blablabla', 8);
+checkPalindrome('blaa lb');
+getNumbersFromString('ababab 2341');
+addSymbols('q', 4, 'we');
