80be191f75e694024a3d3e3e61a4092cdd07c1ba
4rutemu <artyom.denega@gmail.com>
2023-02-19 16:16:40 +0300

    Task 2.28

---
+++ js/functions.js
@@ -0,0 +1,40 @@
+function checkStringLength(str, strLength) {
+    return str.length <= strLength;
+}
+
+function checkPalindrome(str) {
+    let strToCheck = '';
+
+    str = str.replaceAll(' ', '');
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        strToCheck += str[i];
+    }
+
+    return str === strToCheck;
+}
+
+function getNumbersFromString(str) {
+    str = String(str);
+    
+    let number = '';
+    
+    for (let i = 0; i < str.length; i++) {
+        if (Number(str[i]) || str[i] === '0') {
+            number += str[i];
+        }
+    }
+
+    return Number(number);
+}
+
+function addSymbols(str, strLength, addingStr) {
+    
+    while (str.length < strLength) {
+        
+        str = addingStr.slice(0, strLength - str.length) + str;
+        
+    }
+
+    return str;
+}
