9c8939f0f8747aa61e5cff3e23d6aaa87009ac69
4rutemu <artyom.denega@gmail.com>
2023-02-28 20:05:49 +0300

    4.16 lint fix

--- js/main.js
+++ js/main.js
@@ -4,12 +4,15 @@ const COMMENTS = ['Всё отлично!',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+
 const NAMES = ['Стив', 'Глеб', 'Моника', 'Ирина', 'Белла', 'Эдвард', 'Джейкоб', 'Элис', 'Евлампия', 'Агафоник'];
+
 const DESCRIPTIONS = ['Привет, мир! Этот день не был бы таким прекрасным, если бы вы не увидели меня!',
-                      'Вы конечно не в церкви, но Икона перед вами',
-                      'Счастливого мужчину выдает животик, который он отрастил на вкусной домашней еде.',
-                      'Я не злой, просто ты не нравишься мне!',
-                      'С некоторыми людьми даже молчать интересно.'];
+  'Вы конечно не в церкви, но Икона перед вами',
+  'Счастливого мужчину выдает животик, который он отрастил на вкусной домашней еде.',
+  'Я не злой, просто ты не нравишься мне!',
+  'С некоторыми людьми даже молчать интересно.'];
+
 const MIN_POST_ID = 1;
 const MAX_POST_ID = 25;
 const MIN_COMMENT_ID = 1;
@@ -27,7 +30,7 @@ const getRandomInteger = (min, max) => {
   const result = Math.random() * (upper - lower + 1) + lower;
 
   return Math.floor(result);
-}
+};
 
 const createRandomIdFromRangeGenerator = (min, max) => {
   const previousValues = [];
@@ -35,7 +38,6 @@ const createRandomIdFromRangeGenerator = (min, max) => {
   return function () {
     let currentValue = getRandomInteger(min, max);
     if (previousValues.length >= (max - min + 1)) {
-      console.error('Перебраны все числа из диапазона от ' + min + ' до ' + max);
       return null;
     }
     while (previousValues.includes(currentValue)) {
@@ -44,7 +46,7 @@ const createRandomIdFromRangeGenerator = (min, max) => {
     previousValues.push(currentValue);
     return currentValue;
   };
-}
+};
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
@@ -55,8 +57,8 @@ const createPost = () => {
     description: getRandomArrayElement(DESCRIPTIONS),
     likes: getRandomInteger(MIN_LIKES_COUNT, MAX_LIKES_COUNT),
     comments: Array.from({length: getRandomInteger(MIN_COMMENTS_COUNT, MAX_COMMENTS_COUNT)}, createComment())
-  }
-}
+  };
+};
 
 const createComment = () => {
   return {
@@ -64,5 +66,5 @@ const createComment = () => {
     avatar: `img/avatar-${createRandomIdFromRangeGenerator(MIN_AVATAR_ID, MAX_AVATAR_ID)}.svg`,
     message: getRandomArrayElement(COMMENTS),
     name: getRandomArrayElement(NAMES)
-  }
-}
+  };
+};
