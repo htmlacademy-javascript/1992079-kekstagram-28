4c3aad2fff7175a15bb40088e35a15716e513ec1
Keks <keks@htmlacademy.ru>
2023-04-03 18:26:50 +0300

    :heavy_check_mark: Сборка #17

---
+++ 17/js/gallery.js
@@ -0,0 +1,32 @@
+import {posts} from './post-data.js';
+import {showBigPicture} from './big-picture.js';
+
+const galleryElement = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const createPhoto = (prop) => {
+  const element = pictureTemplate.cloneNode(true);
+
+  element.querySelector('img').src = prop.url;
+  element.querySelector('.picture__likes').textContent = prop.likes;
+  element.querySelector('.picture__comments').textContent = prop.comments.length;
+
+  element.addEventListener('click', () => {
+    showBigPicture(prop);
+  });
+
+  return element;
+};
+
+const createFragmentOfPhotos = (props) => {
+  const fragment = document.createDocumentFragment();
+
+  props.forEach((prop) => {
+    const element = createPhoto(prop);
+    fragment.appendChild(element);
+  });
+
+  galleryElement.appendChild(fragment);
+};
+
+createFragmentOfPhotos(posts);
